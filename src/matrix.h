6785473440c452f9472657834b79db6d12f99006
Daniel彡 <jose.saldanafrs@uanl.edu.mx>
2023-05-24 04:49:30 -0500

    Update matrix.h

--- src/matrix.h
+++ src/matrix.h
@@ -13,6 +13,8 @@ typedef struct matrix_t {
 } matrix_t;
 
 void init_matrix(matrix_t* matrix, uint64_t rows, uint64_t columns);
+double get_matrix(matrix_t* matrix, uint64_t row, uint64_t column);
+void write_matrix(matrix_t* matrix, uint64_t row, uint64_t column, double data);
 void fill_matrix_zeroes(matrix_t*);
 void fill_matrix_rand(matrix_t* matrix, double lower_bound, double upper_bound);
 void dot_matrix(matrix_t* matrix_a, matrix_t* matrix_b, matrix_t* matrix_c);
